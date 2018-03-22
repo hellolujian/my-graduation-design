a4e8cb1c59af95b4bf78716201afaf13937f5a37
hellolujian <1370256381@qq.com>
2018-03-22 08:29:57 +0800

    update from vscode

--- share_resource/src/router/index.js
+++ share_resource/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/common/Home'
 
 Vue.use(Router)
 
 export default new Router({
+  linkActiveClass: 'active-class',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
     }
   ]
 })
